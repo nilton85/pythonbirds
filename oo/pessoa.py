c6c5474b11869882f939d26b8d44f1a0c2754937
nilton <niltongoncalves85@gmail.com>
2022-03-29 11:43:42 -0300

    Criado a classe Cumprimentar

--- oo/pessoa.py
+++
@@ -1,9 +0,0 @@
-class Pessoa:
-    def cumprimentar(self):
-        return f'Ola{id(self)}'
-
-if __name__ == '__main__':
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
