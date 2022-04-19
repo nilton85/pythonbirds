2e8bd0101d1c8ff5bd87c20cbb88bd198f8bdeb5
nilton <nilltongoncalves85@gmail.com>
2022-04-19 15:36:16 -0300

    Cridos atributo complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,18 +1,20 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=46):
+    def __init__(self, *filhos, nome=None, idade=46):
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Ola {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Roberto')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Nilton'
-    print(p.nome)
-    print(p.idade)
+    nilton = Pessoa(nome='Nilton')
+    roberto = Pessoa(nilton, nome='Roberto')
+    print(Pessoa.cumprimentar(roberto))
+    print(id(roberto))
+    print(roberto.cumprimentar())
+    print(roberto.nome)
+    print(roberto.idade)
+    for filho in roberto.filhos:
+        print(filho.nome)
